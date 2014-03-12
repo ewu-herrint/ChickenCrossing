1c1edd150e35fd9158b356305fef40dcb82f0e16
ewu-herrint <tyler_herrin@eagles.ewu.edu>
2014-03-11 20:59:07 -0700

    Made node class.

--- ChickenCrossing/src/ChickenCrossing.java
+++ ChickenCrossing/src/ChickenCrossing.java
@@ -3,8 +3,13 @@ public class ChickenCrossing {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-
+		Node root = new Node(null, null, null, null);
+		
+		root.addLeft("Left");
+		root.addRight("Right");
+		
+		System.out.println(root.getLeft().getData());
+		System.out.println(root.getRight().getData());
 	}
 
 }
---
+++ ChickenCrossing/src/Node.java
@@ -0,0 +1,45 @@
+
+
+public class Node
+{
+	private String data;
+	private Node parent;
+	private Node left;
+	private Node right;
+	
+	public Node(String data, Node parent, Node left, Node right)
+	{
+		this.data = data;
+		this.parent = parent;
+		this.left = left;
+		this.right = right;
+	}
+	
+	
+	public void addLeft(String data)
+	{
+		this.left = new Node(data, this, null, null);
+	}
+	
+	public void addRight(String data)
+	{
+		this.right = new Node(data, this, null, null);
+	}
+	
+	public Node getLeft()
+	{
+		return this.left;
+	}
+	
+	public Node getRight()
+	{
+		return this.right;
+	}
+	
+	public String getData()
+	{
+		return this.data;
+	}
+	
+	
+}
