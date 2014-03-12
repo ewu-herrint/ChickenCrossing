f9d46ac4947cc713e890846fc1f7b29ddfbdb49a
Tyler <Tyler@10.104.163.161>
2014-03-12 12:54:48 -0700

    Made more stuff.

--- ChickenCrossing/src/ChickenCrossing.java
+++ ChickenCrossing/src/ChickenCrossing.java
@@ -1,16 +1,95 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class ChickenCrossing {
 
-	public static void main(String[] args) 
+public class ChickenCrossing 
+{
+	public static void main(String[] args) throws FileNotFoundException 
 	{
-		Node root = new Node(null, null, null, null);
+		Node root = MakeResources.makeTree();
+		Node curr = root;
 		
-		root.addLeft("Left");
-		root.addRight("Right");
+		Scanner in = new Scanner(new File("chicken.in"));
+		String results = "";
 		
-		System.out.println(root.getData());
-		System.out.println(root.getLeft().getData());
-		System.out.println(root.getRight().getData());
+		while(in.hasNextLine())
+		{
+			String line = in.nextLine();
+			for(int x = 0; x < line.length(); x++)
+			{			
+				if(line.charAt(x) == '0')
+				{
+					curr = curr.getLeft();
+					if(curr.getData() != null)
+					{
+						results += curr.getData();
+						curr = root;
+					}
+				}
+				else if(line.charAt(x) == '1')
+				{
+					curr = curr.getRight();
+					if(curr.getData() != null)
+					{
+						results += curr.getData();
+						curr = root;
+					}
+				}
+			}
+			results += " ";
+		}
+		
+		int[] coords = getCoords(results);
+		
+		in.close();
+	}
+	
+	private static int[] getCoords(String s)
+	{
+		String[] results = s.split(" ");
+		int[] nums = new int[results.length];
+		int[] coords = new int[results.length/2];
+		
+		for(int x = 0; x < results.length; x++)
+		{
+			nums[x] = getNumber(results[x]);
+		}
+		
+		for(int x = 0; x < nums.length / 2; x = x + 2)
+		{
+			coords[x] = Integer.valueOf(String.valueOf(nums[x]) + String.valueOf(nums[x + 1]));
+		}
+		
+		System.out.println(coords[0] + " " + coords[1]);
+		
+		return coords;
+	}
+	
+	private static int getNumber(String s)
+	{
+		if(s.toUpperCase().compareTo("ZERO") == 0)
+			return 0;
+		if(s.toUpperCase().compareTo("ONE") == 0)
+			return 1;
+		if(s.toUpperCase().compareTo("TWO") == 0)
+			return 2;
+		if(s.toUpperCase().compareTo("THREE") == 0)
+			return 3;
+		if(s.toUpperCase().compareTo("FOUR") == 0)
+			return 4;
+		if(s.toUpperCase().compareTo("FIVE") == 0)
+			return 5;
+		if(s.toUpperCase().compareTo("SIX") == 0)
+			return 6;
+		if(s.toUpperCase().compareTo("SEVEN") == 0)
+			return 7;
+		if(s.toUpperCase().compareTo("EIGHT") == 0)
+			return 8;
+		if(s.toUpperCase().compareTo("NINE") == 0)
+			return 9;
+		
+		throw new RuntimeException("String must be a number zero to nine.");
 	}
 
 }
---
+++ ChickenCrossing/src/MakeResources.java
@@ -0,0 +1,48 @@
+
+public class MakeResources 
+{
+	public static Node makeTree()
+	{
+		Node root = new Node(null, null, null, null); 
+		
+		root.addLeft(null); 	// 0
+		root.addRight(null); 	// 1
+		
+		// MAKE LEFT BRANCH OF TREE
+		Node left = root.getLeft();
+		left.addLeft("U"); 		// 00
+		left.addRight(null); 	// 01
+		
+		Node right = left.getRight();
+		right.addLeft(null);	// 010
+		right.addRight(null); 	// 011
+		
+		left = right.getLeft();
+		left.addLeft("N");		// 0100
+		left.addRight("F");		// 0101
+		
+		right = right.getRight();
+		right.addLeft("T");		// 0110
+		right.addRight("O");	// 0111
+		
+		// MAKE RIGHT BRANCH OF TREE
+		right = root.getRight();
+		right.addLeft("R"); 	// 10
+		right.addRight(null); 	// 11
+		
+		right = right.getRight();
+		right.addLeft(null);	// 110
+		right.addRight(null); 	// 111
+		
+		left = right.getLeft();
+		left.addLeft("V"); 		// 1100
+		left.addRight("W");		// 1101
+		
+		right = right.getRight();
+		right.addLeft("E");		// 1110
+		right.addRight("H");	// 1111
+		
+		
+		return root;
+	}
+}
